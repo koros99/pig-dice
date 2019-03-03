72ba43e31f10e69504d1dedb815fe2bb24493a37
koros99 <koronje254@gmail.com>
2019-03-03 21:17:53 +0300

    Add call back functions for starting the game, rolling dice and holding plays

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,20 @@
-//front-end logic
-$(document).ready(function() {
-  $("button#start").click(function(event) {
-    event.preventDefault();
-    $(".game").fadeIn();
-    $(".landing-page").fadeOut();
-
-    var playerOne = $("input#playerone").val();
-    var playerTwo = $("input#playertwo").val();
-
-    $("#player1").text(playerOne);
-    $("#player2").text(playerTwo);
-
-
-  })
-});
-
 //back-end logic
+var playerOne = {};
+var playerTwo = {};
+
 var rollValue = function() {
-  Math.round(Math.random() * 5) + 1;
+  return Math.floor(6 * Math.random()) + 1;
 }
 
-var Turn = function() {
+function Player(turn, name) {
   this.roll = 0;
   this.turnscore = 0;
   this.totalscore = 0;
-  this.playerName;
+  this.turn = turn;
+  this.playerName = name;
 }
 
-Turn.prototype.checkForOne = function() {
+Player.prototype.checkOne = function() {
   if (this.roll === 1) {
     this.turnscore = 0;
     alert("Sorry " + this.playerName + ", you rolled a 1. Your turn score is zero");
@@ -36,19 +23,72 @@ Turn.prototype.checkForOne = function() {
   }
 };
 
-Turn.prototype.hold = function() {
+Player.prototype.hold = function() {
   this.totalscore += this.turnscore;
   alert(this.playerName + ", your turn's up!")
 }
 
-Turn.prototype.checkForWinner = function() {
+Player.prototype.checkForWinner = function() {
   if (this.totalscore > 99) {
     alert("Congratulations " + this.playerName + "!!! You have won the game")
   }
 };
 
-Turn.prototype.restartGame = function() {
+Player.prototype.restartGame = function() {
   this.roll = 0;
   this.turnscore = 0;
   this.totalscore = 0;
 };
+
+//front-end logic
+$(document).ready(function() {
+  $("button#start").click(function(event) {
+    event.preventDefault();
+
+    var playerOne = new Player();
+    var playerTwo = new Player();
+    $(".game").fadeIn();
+    $(".landing-page").fadeOut();
+
+    var playerOneInput = $("input#playerone").val();
+    var playerTwoInput = $("input#playertwo").val();
+
+    $("#player1").text(playerOneInput);
+    $("#player2").text(playerTwoInput);
+
+    playerOne.playerName = playerOne;
+    playerTwo.playerName = playerTwo;
+  });
+
+  $("button#roll1").click(function(event) {
+    event.preventDefault();
+    playerOne.roll = rollValue();
+    $("#diceroll1").text(playerOne.roll);
+    playerOne.checkone();
+    $("#turnscore1").text(playerOne.turnscore);
+  });
+
+  $("button#roll2").click(function(event) {
+    event.preventDefault();
+    playerTwo.roll = rollValue();
+    $("#diceroll2").text(playerTwo.roll);
+    playerTwo.checkone();
+    $("#turnscore2").text(playerTwo.turnscore);
+  });
+
+  $("button#hold1").click(function(event) {
+    playerOne.hold();
+    $("#total1").text(player1.totalscore);
+    $("#turnscore1").empty();
+    $("diceroll1").empty();
+    player1.checkForWinner();
+  });
+
+  $("button#hold2").click(function(event) {
+    playerOne.hold();
+    $("#total2").text(player2.totalscore);
+    $("#turnscore2").empty();
+    $("diceroll2").empty();
+    player2.checkForWinner();
+  });
+});
