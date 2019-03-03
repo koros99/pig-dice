5da5548d20d13794a4dfac00b9223786932d0943
koros99 <koronje254@gmail.com>
2019-03-03 15:53:00 +0300

    Create a prototype to check for 1

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,22 @@ $(document).ready(function() {
 });
 
 //back-end logic
-
-
 var rollValue = function() {
   Math.round(Math.random() * 5) + 1;
 }
+
+var Turn = function() {
+  this.roll = 0;
+  this.turnscore = 0;
+  this.totalscore = 0;
+  this.playerName;
+}
+
+Turn.prototype.checkForOne = function() {
+  if (this.roll === 1) {
+    this.turnscore = 0;
+    alert("Sorry " + this.playerName + ", you rolled a 1. Your turn score is zero");
+  } else {
+    this.turnscore += this.roll;
+  }
+};
